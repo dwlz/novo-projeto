a975adcef280bc399f3ef29290c607ac485f97ad
dwlz <italomatheusfake@gmail.com>
2022-09-28 01:39:47 -0300

    atualizada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
+
+
 function Home(){
-    return <div>Olá 3</div>
+    return(
+        <div className="container">
+            <h2>Home23</h2>
+            <Contador />
+
+        </div>
+        
+    )
+}
+
+function Contador(){
+    const [contador, setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Add numero</button>
+        </div>
+    )
 }
 
 export default Home
